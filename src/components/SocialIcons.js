1199c93f14fc152bf75c048dadcded48ae3beb9b
mcontoor <mcontoor@gmail.com>
2020-09-05 20:40:02 +0530

    add icons to footer

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+    Codepen,
+    Twitter,
+    Linkedin,
+    GitHub,
+    Instagram,
+    Facebook,
+} from 'react-feather';
+
+const SocialIcons = () => {
+    return (
+        <div className="social-links">
+            <a
+                href="http://twitter.com/MeghnaContoor"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Twitter profile link"
+            >
+                <Twitter />
+            </a>
+            <a
+                href="http://github.com/mcontoor"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="GitHub profile link"
+            >
+                <GitHub />
+            </a>
+            <a
+                href="http://www.linkedin.com/in/meghna-contoor-a6523b80/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LinkedIn Profile link"
+            >
+                <Linkedin />
+            </a>
+            <a
+                href=" http://www.facebook.com/meghna.contoor/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Facebook profile link"
+            >
+                <Facebook />
+            </a>
+            <a
+                href="http://www.instagram.com/meghnacontoor/?hl=en"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Instagram profile link"
+            >
+                <Instagram />
+            </a>
+        </div>
+    );
+};
+
+export default SocialIcons;
