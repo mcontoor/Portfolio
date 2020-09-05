25cc703f6e7148016b3e071a6e0219acb288daa2
mcontoor <mcontoor@gmail.com>
2020-09-05 14:20:04 +0530

    update styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,38 +5,38 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import SEO from "../components/seo"
+import React from 'react';
+import PropTypes from 'prop-types';
+import SEO from '../components/seo';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import '../styles/styles.scss';
 
 const Layout = ({ children }) => {
-  return (
-    <>
-      <SEO title="Meghna Contoor" />
-      <Header />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: "calc(100% - 131px)",
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-      </div>
-      <footer>
-        <div style={{ textAlign: "center" }}>
-          <div>placeholder for icons</div>© {new Date().getFullYear()}
-        </div>
-      </footer>
-    </>
-  )
-}
+    return (
+        <>
+            <SEO title="Meghna Contoor" />
+            <Header />
+            <div
+                style={{
+                    margin: `0 auto`,
+                    maxWidth: 'calc(100% - 131px)',
+                    padding: `0 1.0875rem 1.45rem`,
+                }}
+            >
+                <main>{children}</main>
+            </div>
+            <footer>
+                <div style={{ textAlign: 'center' }}>
+                    <div>placeholder for icons</div>© {new Date().getFullYear()}
+                </div>
+            </footer>
+        </>
+    );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,35 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from '../components/layout';
+import Image from '../components/image';
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <Layout>
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+            }}
+        >
+            <div
+                style={{
+                    maxWidth: `300px`,
+                    marginBottom: `1.45rem`,
+                    height: '200px',
+                    width: '100px',
+                    border: '1px solid #000  ',
+                }}
+            >
+                <Image />
+            </div>
+            <div>
+                <h1>Meghna Contoor</h1>
+                <p>Full Stack Developer</p>
+                <p>Now go build something great.</p>
+            </div>
+        </div>
+    </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
