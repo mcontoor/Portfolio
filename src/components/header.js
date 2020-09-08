c4f280fab34830c32d7ba5273fef19cbc0397b11
mcontoor <mcontoor@gmail.com>
2020-09-08 20:14:01 +0530

    create resume page

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Home, Archive, Clipboard, User } from 'react-feather';
 
 const options = [
     { name: 'Home', path: '/', icon: <Home size={18} /> },
-    { name: 'Resume', path: '/', icon: <Clipboard size={18} /> },
+    { name: 'Resume', path: '/resume', icon: <Clipboard size={18} /> },
     { name: 'Portfolio', path: '/', icon: <Archive size={18} /> },
     { name: 'Contact', path: '/', icon: <User size={18} /> },
 ];
---
+++ src/pages/resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Resume = () => {
+    return (
+        <Layout>
+            <div className="resume-container">
+                <h1>Resume</h1>
+            </div>
+        </Layout>
+    );
+};
+
+export default Resume;
