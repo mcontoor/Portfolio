c4d4011a13f0b1f71b44709646c9dbc7fbe24abb
mcontoor <mcontoor@gmail.com>
2020-09-12 11:51:02 +0530

    refactor form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { navigate } from 'gatsby-link';
+
+function encode(data) {
+    return Object.keys(data)
+        .map(
+            key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+        )
+        .join('&');
+}
 
 const Contact = () => {
+    const [state, setState] = React.useState({});
+
+    const handleChange = e => {
+        setState({ ...state, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': form.getAttribute('name'),
+                ...state,
+            }),
+        })
+            .then(() => navigate(form.getAttribute('action')))
+            .catch(error => alert(error));
+    };
     return (
         <Layout>
             <div className="container contact">
@@ -10,29 +39,54 @@ const Contact = () => {
                     You can write to me on{' '}
                     <a href="mailto:mcontoor@gmail.com">
                         <b>mcontoor@gmail.com</b>
-                        </a>{' '}
+                    </a>{' '}
                     or <br />
                     Drop in a message below! <br />
                 </p>
                 <br />
-                <form name="contact" method="POST" data-netlify="true">
-                    <p>
+                <form
+                    name="contact"
+                    method="post"
+                    // action="/thanks/"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                    onSubmit={handleSubmit}
+                >
+                    {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                    <input type="hidden" name="form-name" value="contact" />
+                    <p hidden>
                         <label>
-                            Your Name:
-                            <br /> <input type="text" name="name" />
+                            Don’t fill this out:{' '}
+                            <input name="bot-field" onChange={handleChange} />
                         </label>
                     </p>
-
                     <p>
                         <label>
-                            Your Email:
-                            <br /> <input type="email" name="email" />
+                            Your name:
+                            <br />
+                            <input
+                                type="text"
+                                name="name"
+                                onChange={handleChange}
+                            />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email:
+                            <br />
+                            <input
+                                type="email"
+                                name="email"
+                                onChange={handleChange}
+                            />
                         </label>
                     </p>
                     <p>
                         <label>
                             Message:
-                            <br /> <textarea name="message"></textarea>
+                            <br />
+                            <textarea name="message" onChange={handleChange} />
                         </label>
                     </p>
                     <p>
