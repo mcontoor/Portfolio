52c4569f6893e34e45e63ee8fcecb23b65cffaa7
mcontoor <mcontoor@gmail.com>
2020-09-13 20:27:34 +0530

    fix resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { resume } from '../resume/resume';
 
 const Resume = () => {
     return (
         <Layout>
             <div className="container">
-                <section id="#skills">
-                    <h1>Tech Stack</h1>
-                </section>
-                <section id="#resume">
+                <section id="resume">
                     <h1>Resume</h1>
+                    <button>Download CV</button>
+                    <ul>
+                        {resume.map(x => (
+                            <>
+                                <li>{x.year}</li>
+                                {x.data.map(y => (
+                                    <div style={{ background: 'yellow' }}>
+                                        <span>
+                                            <h1>{y.name}</h1>
+                                            <h2>{y.location}</h2>
+                                            <h3>{y.title}</h3>
+                                            <h4>{y.date}</h4>
+                                            {y.summary.map(z => (
+                                                <div>
+                                                    <h5>{z.project}</h5>
+                                                    <ul>
+                                                        {z.details.map(a => (
+                                                            <li>{a}</li>
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                            ))}
+                                        </span>
+                                    </div>
+                                ))}
+                            </>
+                        ))}
+                    </ul>
                 </section>
             </div>
         </Layout>
--- src/resume/resume.js
+++ src/resume/resume.js
@@ -1,129 +1,141 @@
 export const resume = [
     {
         year: 2020,
-        data: {
-            name: 'TILL POS',
-            location: 'Hyderabad, India',
-            title: 'Software Engineer',
-            date: "Aug' 19 - Present",
-            summary: [
-                {
-                    project:
-                        'TILL | Powerful Restaurant POS and Management Software, Full Stack Developer',
-                    details: [
-                        'Tech Stack - Node, Express, GraphQL, React, React-Native, AWS, Typescript',
-                        'Implemented the user interface to perfectly match designs using react native to create web, iOS and Android applications and adopted the latest and best react practices in the project.',
-                        'Assist in creating robust web component library for the UI components using Storybook.',
-                        'Use queries and mutations to fetch and update/create data on the fly.',
-                        'Deliver code for server-side programs that is robust, functional, and scalable and build the UI as well.',
-                        'Follow TDD approach to ensure > 90% code coverage',
-                        'Work in an agile environment and participate in team-led solutions, reviewing peer’s code for quality and completeness.',
-                        'Participate in sprint planning and estimating sessions',
-                        'Identify development issues, brainstorm solutions, and constantly improve the solution via testing and feedback until the issue is resolved.',
-                        'Participate in weekly team hackathons, and pair programming exercises.',
-                    ],
-                },
-                {
-                    project:
-                        "Payroo | Payroll Management System for SMSE's in Australia, Full Stack Developer",
-                    details: [
-                        'Tech Stack- Node, Express, DynamoDB, React, Redux, AWS, TypeScript',
-                        'Develop/program new functionalities and features in line with functional and business requirements.',
-                        'Built and shipped new features from v0.2.3 to v0.12.0',
-                        'Built new features and fixed bugs in the frontend and backend in a timely fashion to enable continuous delivery',
-                    ],
-                },
-            ],
-        },
+        data: [
+            {
+                name: 'TILL POS',
+                location: 'Hyderabad, India',
+                title: 'Software Engineer',
+                date: "Aug' 19 - Present",
+                summary: [
+                    {
+                        project:
+                            'TILL | Powerful Restaurant POS and Management Software, Full Stack Developer',
+                        details: [
+                            'Tech Stack - Node, Express, GraphQL, React, React-Native, AWS, Typescript',
+                            'Implemented the user interface to perfectly match designs using react native to create web, iOS and Android applications and adopted the latest and best react practices in the project.',
+                            'Assist in creating robust web component library for the UI components using Storybook.',
+                            'Use queries and mutations to fetch and update/create data on the fly.',
+                            'Deliver code for server-side programs that is robust, functional, and scalable and build the UI as well.',
+                            'Follow TDD approach to ensure > 90% code coverage',
+                            'Work in an agile environment and participate in team-led solutions, reviewing peer’s code for quality and completeness.',
+                            'Participate in sprint planning and estimating sessions',
+                            'Identify development issues, brainstorm solutions, and constantly improve the solution via testing and feedback until the issue is resolved.',
+                            'Participate in weekly team hackathons, and pair programming exercises.',
+                        ],
+                    },
+                    {
+                        project:
+                            "Payroo | Payroll Management System for SMSE's in Australia, Full Stack Developer",
+                        details: [
+                            'Tech Stack- Node, Express, DynamoDB, React, Redux, AWS, TypeScript',
+                            'Develop/program new functionalities and features in line with functional and business requirements.',
+                            'Built and shipped new features from v0.2.3 to v0.12.0',
+                            'Built new features and fixed bugs in the frontend and backend in a timely fashion to enable continuous delivery',
+                        ],
+                    },
+                ],
+            },
+        ],
     },
     {
         year: 2019,
         location: 'Hyderabad, India',
-        data: {
-            name: 'School of Accelerated Learning',
-            title: 'Product Engineering Bootcamp',
-            date: "Feb'19 - May'19",
-            summary: [
-                {
-                    data: [
-                        '3 month long intensive web development bootcamp. Techonologies covered include HTML, CSS, ajax, JQuery, React | nodeJS, mySQL, mongoDB, postgreSQL, redis. Worked on multiple full stack projects with a focus on MERN stack. Individual, group and pair ptogramming.',
-                    ],
-                },
-            ],
-        },
+        data: [
+            {
+                name: 'School of Accelerated Learning',
+                title: 'Product Engineering Bootcamp',
+                date: "Feb'19 - May'19",
+                summary: [
+                    {
+                        details: [
+                            '3 month long intensive web development bootcamp. Techonologies covered include HTML, CSS, ajax, JQuery, React | nodeJS, mySQL, mongoDB, postgreSQL, redis. Worked on multiple full stack projects with a focus on MERN stack. Individual, group and pair ptogramming.',
+                        ],
+                    },
+                ],
+            },
+        ],
     },
     {
         year: 2018,
         location: 'Pune, India',
-        data: {
-            name: 'IISER',
-            title: 'Research Assistant, Glaciology',
-            date: "Aug'18 - Jan'19",
-            summary: [
-                {
-                    project:
-                        'Exploring the statistical properties of supraglacial ponds on debris covered glaciers',
-                    data: [
-                        'Project Student under project titled : “Numerical modelling and field studies of Debris covered glaciers in the Himalayas”. Exploring the statistical properties of thermokarst sinks and attempting to understand their effect on a regional scale in the context of energy/mass balance',
-                        'Automating of data extraction using image processing tools for bulk image analysis, integrating MATLAB and GDAL/OGR commands into shell scripts',
-                        'Statistical analysis of data extracted from satellite images.',
-                    ],
-                },
-            ],
-        },
+        data: [
+            {
+                name: 'IISER',
+                title: 'Research Assistant, Glaciology',
+                date: "Aug'18 - Jan'19",
+                summary: [
+                    {
+                        project:
+                            'Exploring the statistical properties of supraglacial ponds on debris covered glaciers',
+                        details: [
+                            'Project Student under project titled : “Numerical modelling and field studies of Debris covered glaciers in the Himalayas”. Exploring the statistical properties of thermokarst sinks and attempting to understand their effect on a regional scale in the context of energy/mass balance',
+                            'Automating of data extraction using image processing tools for bulk image analysis, integrating MATLAB and GDAL/OGR commands into shell scripts',
+                            'Statistical analysis of data extracted from satellite images.',
+                        ],
+                    },
+                ],
+            },
+        ],
     },
     {
         year: 2016,
         location: 'Mumbai, India',
-        data: {
-            name: 'Oil Field Instrumentation Pvt. Ltd.',
-            title: 'R&D Geologist',
-            date: "Nov'16 - Apr'18",
-            summary: [
-                {
-                    data: [
-                        'Software testing and documentation of mudlogging softwares developed by OFI.',
-                        'Quality control of drilling data, trend charts, logs, and well completion reports.',
-                        'Development of training content, coordination of training batches, and rig crew training on mudlogging principles, techniques, and data acquisition systems.',
-                    ],
-                },
-            ],
-        },
+        data: [
+            {
+                name: 'Oil Field Instrumentation Pvt. Ltd.',
+                title: 'R&D Geologist',
+                date: "Nov'16 - Apr'18",
+                summary: [
+                    {
+                        details: [
+                            'Software testing and documentation of mudlogging softwares developed by OFI.',
+                            'Quality control of drilling data, trend charts, logs, and well completion reports.',
+                            'Development of training content, coordination of training batches, and rig crew training on mudlogging principles, techniques, and data acquisition systems.',
+                        ],
+                    },
+                ],
+            },
+        ],
     },
     {
         year: '2016',
         location: 'Bhutan, India',
-        data: {
-            name: 'Freelance work',
-            title: 'Tour Manager and Soft skills consultant',
-            date: "Dec'14-Apr'16",
-            summary: [
-                {
-                    project: 'Antarang Foundation - Consultant Trainer',
-                    data: [
-                        'Trained 20 underprivileged youth from marginalized communities in Soft skills and spoken English in 2 batches.',
-                        'Conducted Career awareness workshops and nine schools and colleges in Mumbai.',
-                    ],
-                },
-                {
-                    project: 'MakeMyTrip.com - Tour Manager',
-                    data: [
-                        'Coordinating pick up arrivals and check-ins of guests.',
-                        'Brief introduction of destination and expectation setting of guests.',
-                        'Collection and documentation of feedback received to be submitted to head office.',
-                    ],
-                },
-            ],
-        },
+        data: [
+            {
+                name: 'Freelance work',
+                title: 'Tour Manager and Soft skills consultant',
+                date: "Dec'14-Apr'16",
+                summary: [
+                    {
+                        project: 'Antarang Foundation - Consultant Trainer',
+                        details: [
+                            'Trained 20 underprivileged youth from marginalized communities in Soft skills and spoken English in 2 batches.',
+                            'Conducted Career awareness workshops and nine schools and colleges in Mumbai.',
+                        ],
+                    },
+                    {
+                        project: 'MakeMyTrip.com - Tour Manager',
+                        details: [
+                            'Coordinating pick up arrivals and check-ins of guests.',
+                            'Brief introduction of destination and expectation setting of guests.',
+                            'Collection and documentation of feedback received to be submitted to head office.',
+                        ],
+                    },
+                ],
+            },
+        ],
     },
     {
         year: '2014',
         location: 'Mumbai, India',
-        data: {
-            name: "St. Xavier's College",
-            title: 'Masters In Geology',
-            date: '2014',
-            summary: [{ data: ['Completed my masters in Earth Science'] }],
-        },
+        data: [
+            {
+                name: "St. Xavier's College",
+                title: 'Masters In Geology',
+                date: '2014',
+                summary: [{ details: ['Completed my masters in Earth Science'] }],
+            },
+        ],
     },
 ];
